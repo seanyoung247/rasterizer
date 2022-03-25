68e29a0c12dedc8a3c58a34f86a7833760a28bb2
Sean Young <seanyoung247@gmail.com>
2022-03-25 09:04:49 +0000

    feat: Adds initial frame handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 
-import rasterizer from "modules/rasterizer.mjs"
+import { rasterizer } from "./modules/rasterizer.mjs"
 let renderer = null;
 
-(function init() {
+(function () {
     renderer = new rasterizer(document.getElementById("screen-view"));
+    window.requestAnimationFrame(frame);
 })();
 
+function frame() {
+
+    renderer.startFrame();
+        
+    renderer.endFrame();
+    
+    window.requestAnimationFrame(this);
+}
--- assets/js/modules/rasterizer.mjs
+++ assets/js/modules/rasterizer.mjs
@@ -1,13 +1,25 @@
 
 class rasterizer {
-    constructor(screen) {
-        this.screen = screen;
-        this.ctx = screen.getContext("2d");
+    constructor(canvas) {
+        this._canvas = screen;
+        this._context = canvas.getContext("2d");
+        this._buffer = null;
     }
 
+    startFrame() {
+        const width = canvas.clientWidth
+        const height = canvas.clientHeight;
+        this._context.canvas.width = width;
+        this._context.canvas.height = height;
+        // Get screen buffer
+        this._buffer = this._context.getImageData(0,0,width,height);
+    }
     putPixel() {}
     drawLine() {}
     drawTriangle() {}
+    endFrame() {
+        // Copy screen buffer back to screen
+    }
 }
 
 export {rasterizer}
