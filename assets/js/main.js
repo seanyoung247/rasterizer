911bd39e83f004c544dcd8d618900d6a7c235905
Sean Young <seanyoung247@gmail.com>
2022-04-12 15:58:09 +0100

    test: Adds line drawing test code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,18 @@
 
 import { Renderer, Color, RGBA } from "./modules/renderer.mjs"
 let renderer = null;
+let mX = 0;
+let mY = 0;
 
 (function () {
     renderer = new Renderer(document.getElementById("screen-view"));
     window.requestAnimationFrame(frame);
 })();
+
+window.addEventListener('mousedown', e => {
+    mX = e.offsetX;
+    mY = e.offsetY;
+});
 
 function frame() {
 
@@ -16,8 +23,10 @@ function frame() {
             renderer.putPixel(x,y,new Color(RGBA(255,0,0)));
         }
     }
+    
+    renderer.drawLine(100,100,mX,mY,new Color(RGBA(0,0,0,200));
         
     renderer.endFrame();
 
-    //window.requestAnimationFrame(frame);
+    window.requestAnimationFrame(frame);
 }
